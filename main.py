f3bba649e201f78648b269e9ae4992c71dbe6c15
shalevzuriel <shalevz@odem.pro>
2025-05-26 18:08:21 +0300

    commit because of error

--- main.py
+++ main.py
@@ -13,26 +13,3 @@ while True:
     distance = ((time*0.343)/2)/10
     print(distance)
     sleep(100)
-
-    
-    
-''''
-while True:
-    superbit.motor_control(superbit.M1, 255, 0)
-    superbit.motor_control(superbit.M2, 255, 0)
-    superbit.motor_control(superbit.M3, 255, 0)
-    superbit.motor_control(superbit.M4, 255, 0)
-    microbit.sleep(1000)
-
-    superbit.motor_control(superbit.M1, 0, 0)
-    superbit.motor_control(superbit.M2, 0, 0)
-    superbit.motor_control(superbit.M3, 0, 0)
-    superbit.motor_control(superbit.M4, 0, 0)
-    microbit.sleep(1000)
-
-    superbit.motor_control(superbit.M1, -255, 0)
-    superbit.motor_control(superbit.M2, -255, 0)
-    superbit.motor_control(superbit.M3, -255, 0)
-    superbit.motor_control(superbit.M4, -255, 0)
-    microbit.sleep(1000)
-'''
